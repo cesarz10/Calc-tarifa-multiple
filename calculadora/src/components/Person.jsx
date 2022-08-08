23433d72f917d851f9f13b2f135862fec7983864
dosorio55 <dosorio55@gmail.com>
2022-08-08 19:06:22 +0200

    agregado state para new user

--- calculadora/src/components/Person.jsx
+++ calculadora/src/components/Person.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import users from '../Utils/users';
 import User from './logic';
 
 // haciendolo con funciones/objetos
 const Person = () => {
+    ///       estado actual,  funcion para cambiar,   estado inicial
+    let [counter, setCounter] = useState(0)
+    const [userList, setUserList] = useState(users)
 
     // funcion para añadir miembro del grupo 
     const add_member = (name, balance, debt) => {
         const member = new User(name, balance, debt);
         users.push(member)
+        return member
     }
 
     let newUser = { name: "", balance: null };
@@ -16,20 +20,24 @@ const Person = () => {
     const handleNewUser = (event) => {
         const name = event.target.name;
         const value = event.target.value;
+        if (name === "balance" && isNaN(value)) {
+            console.log("necesita ser un número");
+            return
+        }
         newUser = { ...newUser, [name]: value } // ... -> spread operator
     }
 
     const handleSubmit = (event) => {
         event.preventDefault(event)
         add_member(newUser.name, newUser.balance, 0)
-        console.log(users);
+        setUserList([...users])
     }
 
-    console.log(users);
 
     return (
         <>
-            {users.map((user, index) => {
+
+            {userList.map((user, index) => {
                 return (
                     <div key={index}>
                         <p>{`${user.name}, ${user.balance}, ${user.debt}`}</p>
