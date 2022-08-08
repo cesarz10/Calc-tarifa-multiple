da0581ba0a0b0ae779d0c47a0f768c1eea7e62d1
cesarz10 <cesarzr10@gmail.com>
2022-08-08 10:06:44 -0500

    probando branch

---
+++ calculadora/src/components/Person.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+//                                                   intentando con clases
+// class Person {
+    
+//     constructor (name, money, friends, debt) {
+//         this.name = name
+//         this.money = money
+//         this.friends = friends
+//         this.debt = debt
+//     }
+
+
+//     increaseDebt (amount) {
+//         this.debt += amount * this.friends;
+//     }
+
+// }
+
+
+// const Juan = new Person(Juan, 100, 0, 0);
+
+
+// haciendolo con funciones/objetos
+const Person = () => {
+  return (
+    <div>Person</div>
+  )
+}
+
+export default Person
