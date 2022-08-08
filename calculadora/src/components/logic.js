dab857265ad33a2ce1e3e229a26b8eaec7e6c6ff
dosorio55 <dosorio55@gmail.com>
2022-08-08 17:05:16 +0200

    base de trabajo

---
+++ calculadora/src/components/logic.js
@@ -0,0 +1,29 @@
+const user1 = {
+    name: "Juan",
+    money: 2000,
+    debt: 0,
+    group: 3 
+}
+const user2 = {
+    name: "Clara",
+    money: 500,
+    debt: 0,
+    group: 3 
+}
+const user3 = {
+    name: "Gloria",
+    money: 500,
+    debt: 0,
+    group: 3 
+}
+
+const buy1 = {
+    name: "pan",
+    amount: 5,
+    whoPaid: user1
+}
+const buy2 = {
+    name: "pan",
+    amount: 5,
+    whoPaid: [user2, user2]
+}
