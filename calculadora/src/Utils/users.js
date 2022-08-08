8751662934487abb0af6f697613099fdaa45af58
cesarz10 <cesarzr10@gmail.com>
2022-08-08 11:37:06 -0500

    funcion add_new_user en el form

---
+++ calculadora/src/Utils/users.js
@@ -0,0 +1,11 @@
+import User from "../components/logic";
+// import Person from "../components/Person";
+
+
+const Juan = new User("Juan", 200, 0);
+const Pepito = new User("Pepito", 5000, 0);
+const Pepita = new User("Pepita", 10, 0);
+
+const users = [Juan, Pepito, Pepita]
+
+export default users;
--- calculadora/src/components/Person.jsx
+++ calculadora/src/components/Person.jsx
@@ -1,35 +1,41 @@
 import React from 'react'
+import users from '../Utils/users';
 import User from './logic';
 
 // haciendolo con funciones/objetos
 const Person = () => {
-    const Juan = new User("Juan", 200, 0);
 
     // funcion para añadir miembro del grupo 
     const add_member = (name, balance, debt) => {
         const member = new User(name, balance, debt);
-        return member;
+        users.push(member)
     }
 
     let newUser = { name: "", balance: null };
 
     const handleNewUser = (event) => {
         const name = event.target.name;
-        const value = event.target.value
-        newUser = { ...newUser, [name]: value }
+        const value = event.target.value;
+        newUser = { ...newUser, [name]: value } // ... -> spread operator
     }
 
     const handleSubmit = (event) => {
         event.preventDefault(event)
-        const addedMember = add_member(newUser.name, newUser.balance, 0)
-        console.log(addedMember);
+        add_member(newUser.name, newUser.balance, 0)
+        console.log(users);
     }
+
+    console.log(users);
 
     return (
         <>
-            <div>{Juan.balance}</div>
-            <div>{Juan.name}</div>
-            <div>{Juan.debt}</div>
+            {users.map((user, index) => {
+                return (
+                    <div key={index}>
+                        <p>{`${user.name}, ${user.balance}, ${user.debt}`}</p>
+                    </div>
+                )
+            })}
 
             <h3>Add new user</h3>
             <form onSubmit={handleSubmit}>
